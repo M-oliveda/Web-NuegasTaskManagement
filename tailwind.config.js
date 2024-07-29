cf6b67dd1d9089549bb3f7c59f3191a6be33c75b
Mauricio Oliveda <maurioliveda@gmail.com>
2024-07-29 13:29:07 -0300

    :lipstick: Add custom font.
    This commit add the custom font required for this project, inside
    tailwindcss configuration file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+import { default as defaultTheme } from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./web/src/**/*.{html,js,jsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Plus Jakarta Sans", ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
