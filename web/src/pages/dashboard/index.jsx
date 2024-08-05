7bdedd96f03f5f4d2858efa5673e6f2a92b8b975
Mauricio Oliveda <maurioliveda@gmail.com>
2024-08-05 16:13:48 -0300

    :construction: Add tasks summary.
    This commit contains the following implementations:
    
    - It creates a context (and its provider) to work with tasks data.
    - It creates the tasksummary component.
    - It adds the component into the dashboard page.

--- web/src/pages/dashboard/index.jsx
+++ web/src/pages/dashboard/index.jsx
@@ -1,5 +1,42 @@
+import { useContext, useEffect, useState } from "react";
+import TasksContextProvider from "../../components/TasksContextProvider";
+import TaskSummary from "../../components/TaskSummary";
 import Header from "../../layouts/Header";
+import { UserContext } from "../../context/UserContext";
 
 export default function Dashboard() {
-  return <Header />;
+  const { user } = useContext(UserContext);
+  const [isUserInfoLoading, setIsUserInfoLoading] = useState(
+    user ? false : true,
+  );
+
+  useEffect(() => {
+    setIsUserInfoLoading(user ? false : true);
+  }, [user]);
+
+  return (
+    <>
+      <Header />
+      <div className="mx-6 my-8 block xl:hidden">
+        {isUserInfoLoading ? (
+          <>
+            <span className="mb-2 block h-[18] w-[30ch] animate-pulse rounded-[20px] bg-[#546FFF]"></span>
+            <span className="block h-[18] w-[30ch] animate-pulse rounded-[20px] bg-[#546FFF]"></span>
+          </>
+        ) : (
+          <>
+            <p className="text-2xl font-semibold text-secondary">
+              Hi, {user.firstname} {user.lastname}
+            </p>
+            <p className="text-sm font-medium text-secondary-400">
+              Let's finish your task today!
+            </p>
+          </>
+        )}
+      </div>
+      <TasksContextProvider>
+        <TaskSummary />
+      </TasksContextProvider>
+    </>
+  );
 }
