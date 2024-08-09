ed359761192beb63d4879a37873859059372c383
Mauricio Oliveda <maurioliveda@gmail.com>
2024-08-09 14:21:40 -0300

    :alien: Add functionality into tasks services.
    This commits adds a new functionality on tasks service to allow get
    future tasks.

--- web/src/services/tasks.js
+++ web/src/services/tasks.js
@@ -1,5 +1,6 @@
 import { default as tasksDB } from "../data/tasks.json";
 import { default as tasksAnalytics } from "../data/tasksAnalytics.json";
+import { timeLeft } from "../utils";
 
 export default async function getTotalTasks() {
   const totalTasks = tasksDB.length;
@@ -34,4 +35,11 @@ export async function getTasksByDate() {
 
     return { day, completedTasks };
   }, []);
+}
+
+export async function getFutureTasks() {
+  return tasksDB.filter((task) => {
+    const limitDate = new Date(task.timeLimit);
+    return limitDate > Date.now();
+  });
 }
