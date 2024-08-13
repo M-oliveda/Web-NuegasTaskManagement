7397ad192cb1e0a8cb56170b4d3d8720f47d662f
Mauricio Oliveda <maurioliveda@gmail.com>
2024-08-13 11:00:04 -0300

    :bug: Fix Carousel buttons bug.

--- web/src/components/Carousel.jsx
+++ web/src/components/Carousel.jsx
@@ -1,16 +1,32 @@
 import useEmblaCarousel from "embla-carousel-react";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { ArrowLeft2, ArrowRight2 } from "iconsax-react";
 
 export default function Carousel({ children, title }) {
   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false });
+  const [canScrollPrev, setCanScrollPrev] = useState(false);
+  const [canScrollNext, setCanScrollNext] = useState(false);
 
   const scrollPrev = useCallback(() => {
-    if (emblaApi && emblaApi.canScrollPrev()) emblaApi.scrollPrev();
+    if (emblaApi) emblaApi.scrollPrev();
   }, [emblaApi]);
 
   const scrollNext = useCallback(() => {
-    if (emblaApi && emblaApi.canScrollNext()) emblaApi.scrollNext();
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
+
+  useEffect(() => {
+    if (emblaApi) {
+      const updateScrollState = () => {
+        setCanScrollPrev(emblaApi.canScrollPrev());
+        setCanScrollNext(emblaApi.canScrollNext());
+      };
+
+      updateScrollState(); // Update state initially
+      emblaApi.on("select", updateScrollState); // Update state on slide change
+
+      return () => emblaApi.off("select", updateScrollState); // Cleanup listener
+    }
   }, [emblaApi]);
 
   return (
@@ -24,7 +40,7 @@ export default function Carousel({ children, title }) {
             type="button"
             onClick={scrollPrev}
             className="text-secondary disabled:text-secondary-400"
-            disabled={emblaApi && !emblaApi.canScrollPrev()}
+            disabled={!canScrollPrev}
           >
             <ArrowLeft2 />
           </button>
@@ -32,7 +48,7 @@ export default function Carousel({ children, title }) {
             type="button"
             onClick={scrollNext}
             className="text-secondary disabled:text-secondary-400"
-            disabled={emblaApi && !emblaApi.canScrollNext()}
+            disabled={!canScrollNext}
           >
             <ArrowRight2 />
           </button>
